87fe22e0d10e00d8f4560605472eadcd10e5b9d1
Tottadi sai sasank <saisasank2323@gmail.com>
2025-11-10 23:49:14 +0530

    database operation

--- app/Http/Controllers/students.php
+++ app/Http/Controllers/students.php
@@ -4,16 +4,33 @@ namespace App\Http\Controllers;
 use App\Models\student;
 use Illuminate\Http\Request;
 use Illuminate\support\Facades\Http;
+use Illuminate\support\Facades\DB;
 
 class students
 {
     function getstudents()
     {
-        $studentmodel=new \App\Models\student;
-        //or $student = new Student();
-         echo $studentmodel->get_fun();
-        $students = student::all();
-        return view('students',["students"=>$students]);
+        // $studentmodel=new \App\Models\student;
+        // //or $student = new Student();
+        //  echo $studentmodel->get_fun();
+        // $students = student::all();
+        $students =DB::table('student')->get();
+        $wherecom=DB::table('student')->where('id','>',1)->get();
+        $update=DB::table('student')->where('id',3)->update([
+            'name'=>'updated name',
+            'roll_number'=>22,
+            'marks'=>88,
+            'address'=>'updated address'
+        ]);
+        DB::table('student')->insert([
+            'name'=>'new student',
+            'roll_number'=>1,
+            'marks'=>90,
+            'address'=>'new address'
+        ]);
+        DB::table('student')->where('id',4)->delete();
+        echo $students;
+       // return view('students',["students"=>$students]);
         
     }
 
