3a1c384128972cc50ce174cbd3f6053a5ecd4a52
Tottadi sai sasank <saisasank2323@gmail.com>
2025-11-12 23:06:12 +0530

    added all types check

--- routes/web.php
+++ routes/web.php
@@ -8,5 +8,16 @@ Route::get("/getstudents",[students::class,'getstudents']);
 
 Route::get("/getdatafromapi",[students::class,'get_data_from_api']);
 
+
 Route::get("/users",[users::class,'show']);
+
+Route::post("/adduser",[users::class,'adduser']);
+//put and patch both are used to update data all data we use put and some data we use patch
+Route::patch("/edituser",[users::class,'edituser']);
+
+Route::put("/updateuser",[users::class,'updateuser']);
+
+Route::delete("/deleteuser",[users::class,'deleteuser']);
+
+Route::view('/', 'welcome');
 
